b310b70a824b281f6a03be649701de971064e813
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2019-02-06 10:30:35 +0530

    Initial Commit

---
+++ MultiPayIntegraionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiPayIntegraionTest
+//
+//  Created by Abraham VG on 06/02/19.
+//  Copyright © 2019 TechTonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
